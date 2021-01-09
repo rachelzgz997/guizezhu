dd35c2d1639a44890855672960e55bdc1651b19a
rachelzgz997 <a18686728432@163.com>
2021-01-09 20:36:49 +0800

    数据结构联系

---
+++ 乘法.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# sum = 0
+# for i in range(0,101):
+#     sum = sum + i
+#     i = i + 1
+# print(sum)
+
+
+# for i in range(1,10):
+#     print()
+#     for j in range(1,i+1):
+#         print("%d*%d=%d" % (i,j,i*j),end="")
+
+
+# a = [1,2,3]
+# b = a[:]
+# print(b)
+
